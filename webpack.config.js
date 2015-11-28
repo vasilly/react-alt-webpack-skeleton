63572e9169d4bb157c7b75850776e127e0325379
Abi Noda <a@abinoda.com>
2015-11-27 18:23:15 -0600

    wip - extract text plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@
 var webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   path = require('path'),
-  srcPath = path.join(__dirname, 'src');
+  srcPath = path.join(__dirname, 'src'),
+  ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   target: 'web',
@@ -23,11 +24,13 @@ module.exports = {
   },
   module: {
     loaders: [
+      // {test: /\.css|\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'sass-loader')},
       {test: /\.css|\.scss$/, loader: 'style!css!sass'},
       {test: /\.js$|\.jsx$/, exclude: /node_modules/, loader: 'babel?presets[]=es2015,presets[]=stage-0,presets[]=react'}
     ]
   },
   plugins: [
+    // new ExtractTextPlugin("styles.css"),
     new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
     new HtmlWebpackPlugin({
       inject: true,
